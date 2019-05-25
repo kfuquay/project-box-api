071ab58adaca588cf9331c85e6ed34624c42ab8b
kfuquay <kirsty.fuquay@gmail.com>
2019-05-25 19:39:42 -0400

    add cors client_origin

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const {CLIENT_ORIGIN} = require('./config')
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
 
@@ -10,7 +11,7 @@ const app = express();
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
-app.use(cors());
+app.use(cors({origin: CLIENT_ORIGIN}));
 app.use(helmet());
 
 app.get("/api/", (req, res) => {
