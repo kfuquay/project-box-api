a8510debcafadc95af8b0c6235de22c942be47ce
kfuquay <kirsty.fuquay@gmail.com>
2019-05-30 08:51:32 -0400

    initial commit - add authRouter and test

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
-const projectsRouter = require('./projects/projects-router');
-const usersRouter = require('./users/users-router');
+const projectsRouter = require("./projects/projects-router");
+const usersRouter = require("./users/users-router");
+const authRouter = require("./auth/auth-router");
 
 const app = express();
 
@@ -15,8 +16,9 @@ app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
 
-app.use('/api/projects', projectsRouter);
-app.use('/api/users', usersRouter)
+app.use("/api/projects", projectsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/auth", authRouter);
 
 app.get("/api/", (req, res) => {
   res.json({ ok: true });
--- test/test-helpers.js
+++ test/test-helpers.js
@@ -117,8 +117,24 @@ function cleanTables(db) {
   );
 }
 
+function seedUsers(db, users) {
+  const preppedUsers = users.map(user => ({
+    ...user,
+    password: bcrypt.hashSync(user.password, 1),
+  }));
+  return db
+    .into("users")
+    .insert(preppedUsers)
+    .then(() =>
+      // update the auto sequence to stay in sync
+      db.raw(`SELECT setval('users_id_seq', ?)`, [
+        users[users.length - 1].id,
+      ])
+    );
+}
 
 module.exports = {
+  seedUsers,
   cleanTables,
   makeUsersArray,
   makeProjectsArray,
