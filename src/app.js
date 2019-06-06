3a439e5e16f2d68553da14c0efacb8883b92a4d2
kfuquay <kirsty.fuquay@gmail.com>
2019-06-06 15:44:31 -0400

    edit cors:

--- src/app.js
+++ src/app.js
@@ -16,6 +16,25 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(cors({origin: CLIENT_ORIGIN}));
 app.options('*', cors());
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', CLIENT_ORIGIN);
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(helmet());
 
 app.use("/api/projects", projectsRouter);
