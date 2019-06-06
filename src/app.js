b734b729fe6e7b02bb6ea07c04e7999324a66943
kfuquay <kirsty.fuquay@gmail.com>
2019-06-06 16:29:18 -0400

    edit cors

--- src/app.js
+++ src/app.js
@@ -15,23 +15,6 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(cors({ origin: CLIENT_ORIGIN }));
-// app.use(function(req, res, next) {
-//   var oneof = false;
-//   if(req.headers.origin) {
-//       res.header('Access-Control-Allow-Origin', req.headers.origin);
-//       oneof = true;
-//   }
-//   if(req.headers['access-control-request-method']) {
-//       res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-//       oneof = true;
-//   }
-//   if(req.headers['access-control-request-headers']) {
-//       res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-//       oneof = true;
-//   }
-//   if(oneof) {
-//       res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
-//   }
 app.use(function(req, res) {
   res.setHeader(
     "Access-Control-Allow-Origin",
@@ -39,14 +22,7 @@ app.use(function(req, res) {
   );
   res.end()
 });
-// intercept OPTIONS method
-//   if (oneof && req.method == 'OPTIONS') {
-//       res.send(200);
-//   }
-//   else {
-//       next();
-//   }
-// });
+
 
 app.use(helmet());
 
--- src/config.js
+++ src/config.js
@@ -4,4 +4,5 @@ module.exports = {
     DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin:dunder@localhost/project-box',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRTY: process.env.JWT_EXPIRTY || '5h',
+    CLIENT_ORIGIN: 'https://project-box-ims3hchxt.now.sh/'
 }
