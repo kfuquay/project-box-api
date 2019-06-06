612af216637f02778aa55e3175c99d50d965ad64
kfuquay <kirsty.fuquay@gmail.com>
2019-06-06 16:11:21 -0400

    edit cors

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const {CLIENT_ORIGIN} = require('./config');
+const { CLIENT_ORIGIN } = require("./config");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
 const projectsRouter = require("./projects/projects-router");
@@ -14,33 +14,39 @@ const app = express();
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
-app.use(cors({origin: CLIENT_ORIGIN}));
-app.use(function(req, res, next) {
-  var oneof = false;
-  if(req.headers.origin) {
-      res.header('Access-Control-Allow-Origin', req.headers.origin);
-      oneof = true;
-  }
-  if(req.headers['access-control-request-method']) {
-      res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-      oneof = true;
-  }
-  if(req.headers['access-control-request-headers']) {
-      res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-      oneof = true;
-  }
-  if(oneof) {
-      res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
-  }
-
-  // intercept OPTIONS method
-  if (oneof && req.method == 'OPTIONS') {
-      res.send(200);
-  }
-  else {
-      next();
-  }
+app.use(cors({ origin: CLIENT_ORIGIN }));
+// app.use(function(req, res, next) {
+//   var oneof = false;
+//   if(req.headers.origin) {
+//       res.header('Access-Control-Allow-Origin', req.headers.origin);
+//       oneof = true;
+//   }
+//   if(req.headers['access-control-request-method']) {
+//       res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+//       oneof = true;
+//   }
+//   if(req.headers['access-control-request-headers']) {
+//       res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+//       oneof = true;
+//   }
+//   if(oneof) {
+//       res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+//   }
+app.use(function(req, res) {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://project-box.kirstyfuquay.now.sh/"
+  );
 });
+// intercept OPTIONS method
+//   if (oneof && req.method == 'OPTIONS') {
+//       res.send(200);
+//   }
+//   else {
+//       next();
+//   }
+// });
+
 app.use(helmet());
 
 app.use("/api/projects", projectsRouter);
