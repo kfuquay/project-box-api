b4a823e5a2f2815bdfa2e4da597c0201de1b7d7e
kfuquay <kirsty.fuquay@gmail.com>
2019-06-09 09:52:09 -0400

    setup DB_URL for heroku

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin:dunder@localhost/project-box',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin:dunder@localhost/project-box',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRTY: process.env.JWT_EXPIRTY || '5h',
-    // CLIENT_ORIGIN: 'https://project-box-ims3hchxt.now.sh/' 
-     CLIENT_ORIGIN: 'http://localhost:3000'
+    CLIENT_ORIGIN: 'https://project-box-ims3hchxt.now.sh/' 
+    //  CLIENT_ORIGIN: 'http://localhost:3000'
 }
