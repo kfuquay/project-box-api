3de216378a3d60e3bdfb8ab69bb7f143360a5c82
kfuquay <kirsty.fuquay@gmail.com>
2019-06-24 09:47:12 -0400

    remove unused code - function parseBasicToken from  auth-service.js

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -23,10 +23,5 @@ const AuthService = {
       algorithms: ["HS256"]
     });
   },
-  parseBasicToken(token) {
-    return Buffer.from(token, "base64")
-      .toString()
-      .split(":");
-  }
 };
 module.exports = AuthService;
