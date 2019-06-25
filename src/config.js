6921e311dea0a2d97ac7aa30933744e988b6ce90
kfuquay <kirsty.fuquay@gmail.com>
2019-06-25 17:56:26 -0400

    edit client_origin to production origin

--- src/config.js
+++ src/config.js
@@ -6,6 +6,5 @@ module.exports = {
     "postgresql://dunder-mifflin:dunder@localhost/project-box",
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
   JWT_EXPIRTY: process.env.JWT_EXPIRTY || "5h",
-  // CLIENT_ORIGIN: "https://project-box.now.sh",
-  CLIENT_ORIGIN: "http://localhost:3000",
+  CLIENT_ORIGIN: "https://project-box.now.sh",
 };
